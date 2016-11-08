5f5b67e42c0c731df73c52de0f54ae7f2d9824be
Wesley Van Malcot <wesley.vanmalcot@hotmail.com>
2016-11-08 08:48:55 +0100

    Abstract repo config

---
+++ src/main/java/com/realdolmen/service/AirportService.java
@@ -0,0 +1,15 @@
+package com.realdolmen.service;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import com.realdolmen.domain.Airport;
+
+@Remote
+public interface AirportService {
+	
+	List<Airport> findAllAirports();
+
+	Airport findById(Long id);
+}
